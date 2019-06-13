1903c5000cfc826e16173013f3415dc89ebe6163
unknown <felipemantoan@hotmail.com>
2019-06-13 06:42:15 -0300

    Adição do projeto de usuários

---
+++ Javascript projeto usuarios/index.js
@@ -0,0 +1,27 @@
+/*var name = document.querySelector("#exampleInputName");
+//Traga todos os elementos do formulário que tenham nome igual a gender e estejam checados.
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
+*/
+//Seleciona todos os campos do formulário que possuem atributo name
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+var user = {};
+
+fields.forEach(function(field, index){
+
+	if(field.name == "gender"){
+		if(field.checked)
+			user[field.name] = field.value;
+	}else{
+		//Define a chave do objeto user como sendo o nome do campo e atribui o valor como propriedade da chave
+		user[field.name] = field.value;
+	}
+});
+
+console.log(user);
