373aba5d2913b2093119df4d511cc98c0a807387
Fefefx <felipemantoan@hotmail.com>
2020-05-31 18:55:13 -0300

    Remoção de projetos obsoletos

--- Javascript projeto usuarios/classes/Utils.js
+++
@@ -1,8 +0,0 @@
-class Utils{
-	//A palavra static define um método estático
-	static dateFormat(dt){
-		//Retorna a data no formato DD/MM/YYYY hh:mm
-		return dt.getDate()+'/'+(dt.getMonth()+1)+'/'+dt.getFullYear()+' '+dt.getHours()+':'+dt.getMinutes();
-	}
-
-}
